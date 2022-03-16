a5da8e87b5ef784d8014704665a1cb2b29d5e53f
reallyhategithub <castleracer@outlook.com>
2022-03-15 19:18:57 -0600

    progress

--- src/page-load.js
+++ src/page-load.js
@@ -8,28 +8,79 @@ export function pageLoad() {
     document.body.appendChild(content);
     const background = document.createElement('div');
 
-    //Navigation bar, or Header
+    //Navigation bar, or container
     // const headerBackground = document.createElement('div');
     //                     headerBackground.classList.add('header-bar');
 
+    //the matsuya logo
     const matsuyaLogo = new Image();
     matsuyaLogo.src = Logo; 
     matsuyaLogo.classList.add('small-icon')
-    const header = document.createElement('div');
-    header.classList.add('header-bar');
-    header.appendChild(matsuyaLogo);
 
-    const headerSections = document.createElement('div');
-    headerSections.classList.add('header-sections');
-    const headerMenu = document.createElement('p');
-    headerMenu.innerHTML = 'Menu';
-    headerSections.appendChild(headerMenu);
-    const headerStore = document.createElement('p');
-    headerStore.innerHTML = 'Store Locator';
-    headerSections.appendChild(headerStore);
-    // const headerCompany = document.createElement('p');
-    // const headerInvestors = document.createElement('p');
-    // const headerFutureDev = document.createElement('p');
+
+    //main header
+    const mainHeader = document.createElement('header');
+    mainHeader.classList.add('main-header');
+
+    //container
+    const container = document.createElement('div');
+    container.classList.add('container');
+    container.appendChild(matsuyaLogo);
+
+    //navigation bar
+    const mainNav = document.createElement('nav');
+    mainNav.classList.add('main-nav');
+    
+    const ulNav = document.createElement('ul');
+    ulNav.classList.add('main-nav-list');
+    
+    const liNavOne = document.createElement('li');
+    const menuNav = document.createElement('a');
+    menuNav.innerHTML = 'Menu';
+    liNavOne.appendChild(menuNav);
+    ulNav.appendChild(liNavOne);
+    
+    const liNavTwo = document.createElement('li');
+    const storeNav = document.createElement('a');
+    storeNav.innerHTML = 'Store locator';
+    liNavTwo.appendChild(storeNav);
+    ulNav.appendChild(liNavTwo);
+
+    
+    const liNavThree = document.createElement('li');
+    const companyNav = document.createElement('a');
+    companyNav.innerHTML = 'Our company';
+    liNavThree.appendChild(companyNav);
+    ulNav.appendChild(liNavThree);
+    
+    const liNavFour = document.createElement('li');
+    const investorNav = document.createElement('a');
+    investorNav.innerHTML = 'Investor information';
+    liNavFour.appendChild(investorNav);
+    ulNav.appendChild(liNavFour);
+
+    const liNavFive = document.createElement('li');
+    const futureNav = document.createElement('a');
+    futureNav.innerHTML = 'Future development';
+    liNavFive.appendChild(futureNav);
+    ulNav.appendChild(liNavFive);
+    
+    mainNav.appendChild(ulNav);
+    container.appendChild(mainNav);
+
+    // const containerSections = document.createElement('div');
+    // containerSections.classList.add('container-sections');
+    // const containerMenu = document.createElement('p');
+    // containerMenu.innerHTML = 'Menu';
+    // containerSections.appendChild(containerMenu);
+    // const containerStore = document.createElement('p');
+    // containerStore.innerHTML = 'Store Locator';
+    // containerSections.appendChild(containerStore);
+   
+   
+    // const containerCompany = document.createElement('p');
+    // const containerInvestors = document.createElement('p');
+    // const containerFutureDev = document.createElement('p');
 
     //English menu separator
     const separatorDiv = document.createElement('div');
@@ -42,7 +93,44 @@ export function pageLoad() {
     menuLanguageText.classList.add('menu-language-text');
 
     // //Menu divs
-    // const menuContainer = document.createElement('div');
+    const menuContainer = document.createElement('div');
+
+
+    const menuLanguages = document.createElement('div');
+    const menuCenterContainer = document.createElement('div');
+    menuCenterContainer.classList.add('menu-center-container');
+    
+    const menuDiv = document.createElement('div');
+    const menuCurrentLang = document.createElement('div');
+    const menuGridItems = document.createElement('div');
+
+    menuContainer.classList.add('menu-container');
+    menuDiv.classList.add('menu-grid-bg');
+    menuContainer.appendChild(menuDiv);
+    const menuRightHalf = document.createElement('div');
+    menuRightHalf.classList.add('menu-right');
+    menuDiv.appendChild(menuRightHalf);
+
+    const menuLefttHalf = document.createElement('div');
+    menuLefttHalf.classList.add('menu-left');
+    const englishMenu = document.createElement('div');
+    const koreanMenu = document.createElement('div');
+    const spanishMenu = document.createElement('div');
+
+    englishMenu.classList.add('english-menu');
+    koreanMenu.classList.add('korean-menu');
+    spanishMenu.classList.add('spanish-menu');
+    menuLefttHalf.appendChild(englishMenu);
+    menuLefttHalf.appendChild(koreanMenu);
+    menuLefttHalf.appendChild(spanishMenu);
+    menuDiv.appendChild(menuLefttHalf);
+
+    //Selected menu language 
+    const selectedMenuLanguage = document.createElement('div');
+    selectedMenuLanguage.classList.add('selected-menu-lang');
+    selectedMenuLanguage.innerHTML = 'English Menu';
+    menuRightHalf.appendChild(selectedMenuLanguage);  
+    
     // const gyumeshi = document.createElement('div');
     // const curry = document.createElement('div');
     // const bbq = document.createElement('div');
@@ -52,15 +140,21 @@ export function pageLoad() {
     // const toppings = document.createElement('div');
     // const drinks = document.createElement('div');
 
+
+
+
     //class lists
     background.classList.add('background');
 
     // main.appendChild(background);
     content.appendChild(background);
-    background.appendChild(header);
-    header.append(headerSections);
+    background.appendChild(mainHeader);
+    mainHeader.appendChild(container);
+    // container.append(containerSections);
     separatorDiv.appendChild(ramenIcon);
     separatorDiv.appendChild(menuLanguageText);
     background.appendChild(separatorDiv);
+    background.appendChild(menuContainer);
+    
     return content;
 } 
